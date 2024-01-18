3515e3039e42c3780c007ab821cc88a8dfe630a4
Haniehmoh <pl23bay@eduvaud.ch>
2024-01-18 15:43:47 +0100

    first commit

---
+++ Projet Hanieh Mohajerani/Projet Hanieh Mohajerani/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_Hanieh_Mohajerani
+{
+    internal class SpaceShip
+    {
+         
+        private double _x; //creer x
+        private double _y; //creer y
+
+        public void position(double x,double y)
+        {
+            _x = x;
+            _y = y;
+
+        }
+
+
+
+
+    }
+}
